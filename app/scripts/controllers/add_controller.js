13969a3b7dfc641db1d29ebf8c5feb6c11e0f485
John Crocker <johnniecrocker@yahoo.com>
2014-12-03 18:07:06 -0500

    can insert a chart - though not yet with returned data

--- app/scripts/controllers/add_controller.js
+++ app/scripts/controllers/add_controller.js
@@ -18,17 +18,40 @@
             var width = 960,
                 height = 500;
 
-            var x = d3.scale.ordinal()
-                .rangeRoundBands([0, width], .1);
+            //var x = d3.scale.ordinal()
+            //    .rangeRoundBands([0, width], .1);
+            var data = [4, 8, 13, 16, 23, 42];
+
+            var x = d3.scale.linear()
+            .domain([0, d3.max(data)])
+            .range([0, 420]);
 
             var y = d3.scale.linear()
                 .range([height, 0]);
 
-            var chart = d3.select('.chart')
-                .attr('width', width)
-                .attr('height', height);
 
-            console.log(d3.select(results));
+
+                d3.select(".chart")
+                .selectAll("div")
+                .data(data)
+                .enter().append("div")
+                .style("width", function(d) { return x(d) + "px"; })
+                .text(function(d) { return d; });
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient('bottom');
+
+        //    chart.append('x')
+          //      .attr('class', 'x axis')
+                //.attr('transform', 'translate(0,' + height')')
+            //    .call(xAxis);
+
+            console.log(d3.selectAll(results));
+
+    //        d3.select(results, function(error, data){
+    //          console.log("I'm in the d3.select function");
+    //        })
 
           });
 

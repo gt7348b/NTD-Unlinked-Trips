bbbe1a4ff079d351e3a45af4b73df33913fe5e71
John Crocker <johnniecrocker@yahoo.com>
2014-12-04 10:22:29 -0500

    merged with dev

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,9 @@ console.log('Roscoes a cutie');
 
   angular.module('UPT', ['ngResource','ngRoute'])
     .constant({
-      'DATA_SOURCE': src='data/UPT1.json'
+      'DATA_SOURCE': src='data/UPT_2.json'
     })
+
 
     .config(function($routeProvider){
 
@@ -34,6 +35,11 @@ console.log('Roscoes a cutie');
         controller: 'AddController'
       });
 
+      $routeProvider.when('/results', {
+        templateUrl: 'templates/search_results.html',
+        controller: 'AddController'
+      });
+
 
 
 
@@ -49,17 +55,59 @@ console.log('Roscoes a cutie');
 (function(){
 
   angular.module('UPT')
-    .controller('AddController', ['$scope', 'DataFactory',
-      function($scope, DataFactory){
+    .controller('AddController', ['$scope', '$location','$rootScope','DataFactory',
+      function($scope, $location, $rootScope, DataFactory){
+
+        var agency_response = [],
+        msa_response = [],
+        mode_response = [];
 
         console.log("hey - i'm in the add controller")
 
         $scope.searchAgency = function(agency){
-          DataFactory.searchAgency(agency);
+          DataFactory.searchAgency(agency).then(function(results){
+
+            $scope.agencies = results;
+
+            var width = 960,
+                height = 500;
+
+            var x = d3.scale.ordinal()
+                .rangeRoundBands([0, width], .1);
+
+            var y = d3.scale.linear()
+                .range([height, 0]);
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient('bottom');
+
+                d3.select(".chart")
+                .selectAll("div")
+                .data(results)
+                .enter().append("div")
+                .style("width", function(d) { return y(d) + "px"; })
+                .text(function(d) { return d; });
+
+        //    chart.append('x')
+          //      .attr('class', 'x axis')
+                //.attr('transform', 'translate(0,' + height')')
+            //    .call(xAxis);
+
+            console.log(d3.selectAll(results));
+
+    //        d3.select(results, function(error, data){
+    //          console.log("I'm in the d3.select function");
+    //        })
+
+          });
+
         };
 
         $scope.searchMSA = function(msa){
-          DataFactory.searchMSA(msa);
+          DataFactory.searchMSA(msa).then(function(results){
+            $scope.agencies = results;
+          });
         };
 
         $scope.selectModes = function(modes){
@@ -76,24 +124,74 @@ console.log('Roscoes a cutie');
 (function(){
 
   angular.module('UPT')
-    .factory('DataFactory', ['$resource','DATA_SOURCE',
-      function($resource, DATA_SOURCE){
+    .factory('DataFactory', ['$q','$location','DATA_SOURCE',
+      function($q, $location, DATA_SOURCE){
 
 
        var searchAgency = function(agency){
-         console.log(agency);
-         $http.get('data/UPT1.json').success(function(data){
-           console.log(data);
+           console.log(agency);
+
+           var response = [];
+           var deferred = $q.defer();
+
+           $.getJSON(DATA_SOURCE).done(function(ntd_data){
+
+             var sel_agency = ntd_data.filter(function(entry){ //filters the data by agency name
+               if (entry.AGENCY == agency.name){
+                 response.push(entry);  //adds the data to the response array
+                 }
+                });
+
+             console.log(response);
+             //return response;
+             deferred.resolve(response);
+
+           //}).done(function(response){
+             //return $location.path('/results');
+
          });
+
+         return deferred.promise;
        };
 
        var searchMSA = function(msa){
          console.log(msa);
+
+         var msa_filter = [];
+         var deferred = $q.defer();
+
+         $.getJSON(DATA_SOURCE).done(function(ntd_data){
+
+           ntd_data.forEach(function(entry){
+
+             if (entry.UZA_NAME == msa.region){
+
+               msa_filter.push(entry); //Adds results to array
+
+             }   // This is the return of the if
+           });
+           console.log(msa_filter);
+           //return msa_filter;
+           deferred.resolve(msa_filter);
+         });
+         return deferred.promise;
        };
 
+
       var selectModes = function(modes){
+
         console.log(modes);
-        console.log(DATA_SOURCE);
+
+        var data = $.getJSON(DATA_SOURCE).done(function(ntd_data){
+
+        //  ntd_data.forEach(function(entry){
+
+          //  if (entry.UZA_NAME == modes.name)
+
+          //});
+
+        });
+
       };
 
 
@@ -108,7 +206,10 @@ console.log('Roscoes a cutie');
 }());
 
 (function(){}(
+//  angular.module('UPT')
+  //  .controller('ModeController', [$scope, function($scope){
 
+    //}])
 
 ));
 

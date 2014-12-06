d83f1d87c0b30d11182db9e110040d93f5959f4c
John Crocker <johnniecrocker@yahoo.com>
2014-12-06 16:01:39 -0500

    added legend text

--- app/data/data_factory.js
+++ app/data/data_factory.js
@@ -9,7 +9,7 @@
 
            var response = [];
            var deferred = $q.defer();
-           
+
            var data = d3.csv(DATA_SOURCE, function(error, data){
 
              var sel_agency = data.filter(function(entry){ //filters the data by agency name
@@ -34,8 +34,15 @@
 
              //  console.log(month);
 
-             // This creates an array used to call render the data
+            var modes = response.map(function(m){
+              return {
+                mode: m.Modes
+              }
 
+            });
+
+             console.log(modes);
+              // This creates an array used to call render the data
              var uptData = response.map(function (t){
                return {
                  agency: t.Agency,
@@ -54,6 +61,8 @@
              cleandata.push(uptData);
 
              cleandata.push(month);
+
+             cleandata.push(modes);
 
              deferred.resolve(cleandata);
 
@@ -101,74 +110,12 @@
 
         });
 
-
       };
-
-      var d3render = function(results){
-
-        var response = [];
-        var deferred = $q.defer();
-
-        console.log('I made into into the Render Factory');
-
-        var data = d3.csv('data/September 2014 Adjusted Database/UPT-Table 1.csv', function(error, data){
-
-          var sel_agency = data.filter(function(entry){ //filters the data by agency name
-            if (entry.Agency == results.name){
-              response.push(entry);  //adds the data to the response array
-            }
-          });
-
-          //console.log(response);
-
-          // This creates an array of months
-
-          var month = d3.keys(data[0])
-              .filter(function(key){ return key!=='Modes'})
-              .filter(function(key){ return key!=='NTDID'})
-              .filter(function(key){ return key!=='Agency'})
-              .filter(function(key){ return key!=='Active'})
-              .filter(function(key){ return key!=='SSW'})
-              .filter(function(key){ return key!=='UZA'})
-              .filter(function(key){ return key!=='UZA Name'})
-              .filter(function(key){ return key!=='TOS'});
-
-            //  console.log(month);
-
-            // This creates an array used to call render the data
-
-          var uptData = response.map(function (t){
-            return {
-              agency: t.Agency,
-              mode:   t.Modes,
-              region: t.UZA,
-              trips: month.map(function(d){
-                return {month: d, upt: +t[d].replace(/,/g, '')}; //returns array of month and unlinked passenger trips as number
-              })
-            };
-          });
-
-          console.log(uptData);
-
-          var cleandata = [];
-
-          cleandata.push(uptData);
-
-          cleandata.push(month);
-
-          deferred.resolve(cleandata);
-
-        });
-
-        return deferred.promise;
-      };
-
 
       return {
         searchAgency: searchAgency,
         searchMSA:    searchMSA,
         selectModes:  selectModes,
-        d3render:     d3render,
         }
 
     }]);
--- app/scripts/controllers/add_controller.js
+++ app/scripts/controllers/add_controller.js
@@ -10,89 +10,6 @@
 
         console.log("hey - i'm in the add controller")
 
-        $scope.d3render = function(results){
-
-          DataFactory.d3render(results).then(function(results){
-
-            $scope.agencies = results[0];
-
-            var uptData = results[0];
-
-            //console.log(results);
-
-            var month = results[1];
-            //console.log(month)
-
-            //d3.select(".chart").append("svg").attr("width", 100).attr("height", 100).append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25).style("fill", "purple");
-
-            var margin = {top: 20, right: 30, bottom: 30, left: 40},
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
-
-
-            var x = d3.scale.ordinal()
-            .rangeRoundBands([0, width], .1);
-
-            var y = d3.scale.linear()
-            .range([height, 0]);
-
-            var line = d3.svg.line()
-            .interpolate('cardinal')
-            .x(function(d) { return x(d.label)})
-            .y(function(d) { return y(d.value)});
-
-            var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient('bottom');
-
-            var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient('left');
-
-            var line = d3.svg.line()
-            .interpolate("cardinal")
-            .x(function (d) { return x(d.month) + x.rangeBand() / 2; })
-            .y(function (d) { return y(d.upt); });
-
-            var color = d3.scale.ordinal()
-            .range(["#001c9c","#101b4d","#475003","#9c8305","#d3c47c"]);
-
-            var svg = d3.select('.chart').append('svg')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-            .append('g')
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-            // This renders defines the axes
-
-            x.domain(month.map(function(d){ return d }));
-            y.domain([
-              d3.min(uptData, function(c){
-                return d3.min(c.trips, function(min){ return min.upt;});
-              }),
-              d3.max(uptData, function(c){
-                return d3.max(c.trips, function(max){ return max.upt;});
-              })
-              ]);
-
-            // This renders the data
-
-            var trips = svg.selectAll('.chart')
-            .data(uptData)
-            .enter().append('g')
-            .attr('class', 'chart');
-
-            trips.append('path')
-            .attr('class', 'line')
-            .attr('d', function(d) {return line(d.trips);})
-            .style('stroke', function(d) { return color(d.Modes);})
-            .style('stroke-width', '.2em')
-            .style('fill', 'none');
-
-          });
-        };
-
-
         $scope.searchAgency = function(agency){
           DataFactory.searchAgency(agency).then(function(results){
             $scope.agencies = results[0];
@@ -101,8 +18,17 @@
 
             var month = results[1];
 
-            var margin = {top: 20, right: 30, bottom: 30, left: 40},
-            width = 960 - margin.left - margin.right,
+            var modes = ['CR', 'DR', 'HR', 'LR', 'MB', 'FB', 'TB'];
+            console.log(modes);
+
+            var color = d3.scale.ordinal()
+            .range(["#001c9c","#101b4d","#475003","#9c8305","#d3c47c", 'green', 'steelblue']);
+
+
+            color.domain(modes);
+
+            var margin = {top: 20, right: 50, bottom: 30, left: 75},
+            width = 1500 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
 
             var x = d3.scale.ordinal()
@@ -127,13 +53,15 @@
             var color = d3.scale.ordinal()
             .range(["#001c9c","#101b4d","#475003","#9c8305","#d3c47c"]);
 
+            // This creates the svg object
+
             var svg = d3.select('.chart').append('svg')
             .attr('width', width + margin.left + margin.right)
             .attr('height', height + margin.top + margin.bottom)
             .append('g')
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            // This renders defines the axes
+            // This defines the axes
 
             x.domain(month.map(function(d){ return d }));
             y.domain([
@@ -145,9 +73,11 @@
               })
               ]);
 
+            // This renders the axes
+
             svg.append('g')
                 .attr('class', 'x axis')
-                .attr('transformt', 'translate (0, ' + height + ')')
+                .attr('transform', 'translate (0, ' + height + ')')
                 .call(xAxis);
 
             svg.append('g')
@@ -158,7 +88,7 @@
                 .attr('y', 6)
                 .attr('dy', '.71em')
                 .style('text-anchor', 'end')
-                .text('Number of Rounds');
+                .text('Number of Unlinked Passenger Trips');
 
               // This renders the data
 
@@ -173,6 +103,15 @@
               .style('stroke', function(d) { return color(d.Modes);})
               .style('stroke-width', '.2em')
               .style('fill', 'none');
+
+              var legend = svg.selectAll('.legend')
+                  .data(modes.slice().reverse())
+                .enter().append('g')
+                  .attr('class', 'legend')
+                  .attr('transform', function(d, i){
+                    return 'translate(55, ' + i * 20 + ')';
+                  });
+
 
           });
 

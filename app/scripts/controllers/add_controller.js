4b4fc02652c3965869531fa6a794ebdd189fc227
John Crocker <johnniecrocker@yahoo.com>
2014-12-10 20:04:13 -0500

    adding rendering

--- app/scripts/controllers/add_controller.js
+++ app/scripts/controllers/add_controller.js
@@ -59,6 +59,8 @@
                 .x(function (d) { return x(d.month) + x.rangeBand() / 2; })
                 .y(function (d) { return y(d.upt); });
 
+            //This defines things for a stacked area
+
             var stack = d3.layout.stack()
                 .offset('zero')
                 .values(function(d){return d.trips; })
@@ -234,6 +236,136 @@
                 DataFactory.tripsPerhour(uptData, month, vrhData).then(function(results){
 
                   console.log(results);
+
+                  var tphData = results;
+
+                  //This defines overall rendering variables
+                  var modes = ['CR', 'DR', 'HR', 'LR', 'MB', 'FB', 'TB'];
+
+                  var color = d3.scale.ordinal()
+                  .range(["#001c9c","hsla(113, 86%, 50%, 1)","hsla(360, 100%, 50%, 1)","hsla(275, 100%, 44%, 1)","hsla(247, 100%, 50%, 1)", 'green', 'steelblue']);
+
+                  color.domain(modes);
+
+                  var margin = {top: 20, right: 50, bottom: 50, left: 60},
+                  width = 1200 - margin.left - margin.right,
+                  height = 500 - margin.top - margin.bottom;
+
+                  var x = d3.scale.ordinal()
+                  .rangeRoundBands([0, width], .1);
+
+                  var y = d3.scale.linear()
+                  .range([height, 0]);
+
+                  var xAxis = d3.svg.axis()
+                  .scale(x)
+                  .orient('bottom');
+
+                  var yAxis = d3.svg.axis()
+                  .scale(y)
+                  .orient('left');
+
+                  var line = d3.svg.line()
+                  .interpolate("cardinal")
+                  .x(function (d) { return x(d.month) + x.rangeBand() / 2; })
+                  .y(function (d) { return y(d.tph); });
+
+                  // This creates the svg object
+
+                  var svg = d3.select('.chart').append('svg')
+                  .attr('width', width + margin.left + margin.right)
+                  .attr('height', height + margin.top + margin.bottom)
+                  .append('g')
+                  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                  // This defines the axes
+
+                  x.domain(month.map(function(d){ return d }));
+
+                  // This is for line chart
+                  y.domain([
+                    d3.min(uptData, function(c){
+                      return d3.min(c.monthlytph, function(min){ return min.tph;});
+                    }),
+                    d3.max(uptData, function(c){
+                      return d3.max(c.monthlytph, function(max){ return max.tph;});
+                    })
+                    ]);
+
+                  // This renders the axes
+
+                  svg.append('g')
+                  .attr('class', 'x axis')
+                  .attr('transform', 'translate (0, ' + height + ')')
+                  .call(xAxis)
+                  .selectAll('text')
+                  .style('text-anchor', 'end')
+                  .style('font-size', '.45em')
+                  .attr('dx', '-.8em')
+                  .attr('dy', '.15em')
+                  .attr('transform', function(d){
+                    return 'rotate(-65)'
+                  });
+
+                  svg.append('g')
+                  .attr('class', 'y axis')
+                  .call(yAxis)
+                  // .selectAll('text')
+                  // .style('font-size', '.575em')
+                  .append('text')
+                  .attr('transform', 'rotate(-90)')
+                  .attr('y', 6)
+                  .attr('dy', '.71em')
+                  .style('text-anchor', 'end')
+                  .text('Trips per Vehicle Revenue Miles');
+
+                  // This renders the grid
+
+                  svg.selectAll('line.y')
+                  .data(y.ticks(10))
+                  .enter().append('line')
+                  .attr('class', 'y')
+                  .attr('x1', 0)
+                  .attr('x2', width)
+                  .attr('y1', y)
+                  .attr('y2', y)
+                  .style('stroke', '#ccc');
+
+                  // This section renders a line chart
+                  var trips = svg.selectAll('.chart')
+                  .data(uptData)
+                  .enter().append('g')
+                  .attr('class', 'chart');
+
+                  trips.append('path')
+                  .attr('class', 'line')
+                  .attr('d', function(d) {return line(d.monthlytph);})
+                  .style('stroke', function(d) { return color(d.mode);})
+                  .style('stroke-width', '.2em')
+                  .style('fill', 'none');
+
+                  // This creates the legend
+                  var legend = svg.selectAll('.legend')
+                  .data(modes.slice().reverse())
+                  .enter().append('g')
+                  .attr('class', 'legend')
+                  .attr('transform', function(d, i){
+                    return 'translate(25, ' + i * 30 + ')';
+                  });
+
+                  legend.append('rect')
+                  .attr('x', width - 10)
+                  .attr('width', 10)
+                  .attr('height', 10)
+                  .style('fill', color)
+                  .style('stroke', 'grey');
+
+                  legend.append('text')
+                  .attr('x', width - 12)
+                  .attr('y', 6)
+                  .attr('dy', '.5em')
+                  .style('text-anchor', 'end')
+                  .text(function(d){ return d;});
 
                 });
 

7b5ac96d526e0d2cfcfecb19fd4b1d9f852ec420
John Crocker <johnniecrocker@yahoo.com>
2014-12-10 21:03:06 -0500

    gulped

--- scripts/main.js
+++ scripts/main.js
@@ -113,6 +113,8 @@ console.log('Roscoes a cutie');
                 .x(function (d) { return x(d.month) + x.rangeBand() / 2; })
                 .y(function (d) { return y(d.upt); });
 
+            //This defines things for a stacked area
+
             var stack = d3.layout.stack()
                 .offset('zero')
                 .values(function(d){return d.trips; })
@@ -124,6 +126,10 @@ console.log('Roscoes a cutie');
                 .x(function (d) { return x(d.month) + x.rangeBand() / 2; })
                 .y0(function (d) { return y(d.y0); })
                 .y1(function (d) { return y(d.y0 + d.y); });
+
+            var stackmode = function(c){
+              return c.mode
+            };
 
 
             stack(tripsArr);
@@ -143,9 +149,9 @@ console.log('Roscoes a cutie');
             x.domain(month.map(function(d){ return d }));
 
             //This is for stacked bar
-            //y.domain([0, d3.max(tripsArr, function(d){return d.total})]);
+          //  y.domain([0, d3.max(tripsArr, function(d){return d.total})]);
 
-            // This is for line chart
+            // // This is for line chart
             y.domain([
               d3.min(uptData, function(c){
                 return d3.min(c.trips, function(min){ return min.upt;});
@@ -213,7 +219,7 @@ console.log('Roscoes a cutie');
 
 
               // console.log(tripsArr);
-              // // This section renders a stacked area
+              // This section renders a stacked area
               // var trips = svg.selectAll('.chart')
               //     .data(tripsArr)
               //     .enter().append('g')
@@ -222,7 +228,7 @@ console.log('Roscoes a cutie');
               // trips.append('path')
               //     .attr('class', 'stackPath')
               //     .attr('d', function (d) { return area(d.trips); })
-              //     .style('fill', function (d) {return color(d.month); })
+              //     .style('fill', function (d) {return color(stackmode(d.trips)); })
               //     .style('stroke', 'grey');
 
               // This renders stacked bar NOT WORKING
@@ -285,7 +291,144 @@ console.log('Roscoes a cutie');
               DataFactory.vehicleHours(agency).then(function(results){
                 var vrhData = results[3];
 
-                DataFactory.tripsPerhour(uptData, month, vrhData);
+                DataFactory.tripsPerhour(uptData, month, vrhData).then(function(results){
+
+                  console.log(results);
+
+                  var tphData = results;
+
+                  //This defines overall rendering variables
+                  var modes = ['CR', 'DR', 'HR', 'LR', 'MB', 'FB', 'TB'];
+
+                  var color = d3.scale.ordinal()
+                  .range(["#001c9c","hsla(113, 86%, 50%, 1)","hsla(360, 100%, 50%, 1)","hsla(275, 100%, 44%, 1)","hsla(247, 100%, 50%, 1)", 'green', 'steelblue']);
+
+                  color.domain(modes);
+
+                  var margin = {top: 20, right: 50, bottom: 50, left: 60},
+                  width = 1200 - margin.left - margin.right,
+                  height = 500 - margin.top - margin.bottom;
+
+                  var x = d3.scale.ordinal()
+                  .rangeRoundBands([0, width], .1);
+
+                  var y = d3.scale.linear()
+                  .range([height, 0]);
+
+                  var xAxis = d3.svg.axis()
+                  .scale(x)
+                  .orient('bottom');
+
+                  var yAxis = d3.svg.axis()
+                  .scale(y)
+                  .orient('left');
+
+                  var line = d3.svg.line()
+                  .interpolate("cardinal")
+                  .x(function (d) { if (d !== undefined){return x(d.month) + x.rangeBand() / 2; }})
+                  .y(function (d) { if (d !== undefined){return y(d.tph); }});
+
+                  // This creates the svg object
+
+                  var svg = d3.select('.chart').append('svg')
+                  .attr('width', width + margin.left + margin.right)
+                  .attr('height', height + margin.top + margin.bottom)
+                  .append('g')
+                  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                  // This defines the axes
+
+                  x.domain(month.map(function(d){ return d }));
+
+                  // This is for line chart
+                  y.domain([
+                    d3.min(tphData, function(c){
+                        return d3.min(c.monthlytph, function(min){
+                          if (min !== undefined){
+                          return min.tph;}});
+                      }),
+                    d3.max(tphData, function(c){
+                            return d3.max(c.monthlytph, function(max){
+                              if (max !== undefined){return max.tph;}});
+                        })
+                    ]);
+
+                  // This renders the axes
+
+                  svg.append('g')
+                  .attr('class', 'x axis')
+                  .attr('transform', 'translate (0, ' + height + ')')
+                  .call(xAxis)
+                  .selectAll('text')
+                  .style('text-anchor', 'end')
+                  .style('font-size', '.45em')
+                  .attr('dx', '-.8em')
+                  .attr('dy', '.15em')
+                  .attr('transform', function(d){
+                    return 'rotate(-65)'
+                  });
+
+                  svg.append('g')
+                  .attr('class', 'y axis')
+                  .call(yAxis)
+                  // .selectAll('text')
+                  // .style('font-size', '.575em')
+                  .append('text')
+                  .attr('transform', 'rotate(-90)')
+                  .attr('y', 6)
+                  .attr('dy', '.71em')
+                  .style('text-anchor', 'end')
+                  .text('Trips per Vehicle Revenue Miles');
+
+                  // This renders the grid
+
+                  svg.selectAll('line.y')
+                  .data(y.ticks(10))
+                  .enter().append('line')
+                  .attr('class', 'y')
+                  .attr('x1', 0)
+                  .attr('x2', width)
+                  .attr('y1', y)
+                  .attr('y2', y)
+                  .style('stroke', '#ccc');
+
+                  // This section renders a line chart
+                  var trips = svg.selectAll('.chart')
+                  .data(tphData)
+                  .enter().append('g')
+                  .attr('class', 'chart');
+
+                  trips.append('path')
+                  .attr('class', 'line')
+                  .attr('d', function(d) {return line(d.monthlytph);})
+                  .style('stroke', function(d) { return color(d.mode);})
+                  .style('stroke-width', '.2em')
+                  .style('fill', 'none');
+
+                  // This creates the legend
+                  var legend = svg.selectAll('.legend')
+                  .data(modes.slice().reverse())
+                  .enter().append('g')
+                  .attr('class', 'legend')
+                  .attr('transform', function(d, i){
+                    return 'translate(25, ' + i * 30 + ')';
+                  });
+
+                  legend.append('rect')
+                  .attr('x', width - 10)
+                  .attr('width', 10)
+                  .attr('height', 10)
+                  .style('fill', color)
+                  .style('stroke', 'grey');
+
+                  legend.append('text')
+                  .attr('x', width - 12)
+                  .attr('y', 6)
+                  .attr('dy', '.5em')
+                  .style('text-anchor', 'end')
+                  .text(function(d){ return d;});
+
+                });
 
               });
           });
@@ -618,8 +761,6 @@ console.log('Roscoes a cutie');
                }
              });
 
-             //console.log(response);
-
              // This creates an array of months
 
              var month = d3.keys(data[0])
@@ -632,13 +773,12 @@ console.log('Roscoes a cutie');
              .filter(function(key){ return key!=='UZA Name'})
              .filter(function(key){ return key!=='TOS'});
 
-             //  console.log(month);
 
+             //This section will create an object of modes for rendering color
             var modes = response.map(function(m){
               return {
                 mode: m.Modes
               }
-
             });
 
 
@@ -653,13 +793,9 @@ console.log('Roscoes a cutie');
             })
 
             response.forEach(function(d){
-
               month.map(function(trips){
-
                 tripmonth[trips].trips.push({mode: d.Modes, month: trips, upt: +d[trips].replace(/,/g, '')});
-
               });
-
             });
 
               // This creates an array used to call render the data
@@ -674,8 +810,7 @@ console.log('Roscoes a cutie');
                };
              });
 
-             //console.log(uptData);
-
+             // This pushes the data that will be returned to the controller for rendering
              var cleandata = [];
 
              cleandata.push(uptData);
@@ -697,57 +832,39 @@ console.log('Roscoes a cutie');
        };
 
        var tripsPerhour = function(uptData, month, vrhData){
-        //  console.log(uptData);
-        //  console.log(vrhData);
-        //  console.log(month);
 
-        //  var uptnumeric = uptData.map(function(t){
-        //    return {
-        //     agency: t.Agency,
-        //     mode: t.Modes,
-        //     monthtrips: month.map(function(d){
-        //       return{month: d, trips: +t[d].replace(/,/g, "")}
-        //     })
-        //    }
-        //  });
-         //
-        //  var vrhnumeric = vrhData.map(function(t){
-        //   return {
-        //      agency: t.Agency,
-        //      mode: t.Modes,
-        //      monthhours: month.map(function(d){
-         //
-        //        if (t[d] !== undefined){
-        //          return {month: d, hours: +t[d].replace(/,/g, "")}
-        //       }
-        //      })
-        //    }
-        //  });
-        //  console.log(uptnumeric);
-        //  console.log(vrhnumeric);
+         var response = [];
+         var deferred = $q.defer();
 
+         //This section creates the array and caluculates Unlinked Trips per Vehicle Revenue Hour
+         var tripsphour = vrhData.map(function(t){
+             var trips = [];
+             uptData.forEach(function(upt){
+               if (upt.Modes === t.Modes){
+                 trips.push(upt);
+               }
+             });
 
-         var calctripspervrh = function(tripsArr, hoursArr){
-           console.log(tripsArr);
-           console.log(hoursArr);
+            return {
+               agency: t.Agency,
+               mode: t.Modes,
+               monthlytph: month.map(function(d){
 
-           return {
-            agency: tripsArr.Agency,
-            region: tripsArr.UZA,
-            mode: tripsArr.Modes,
-            tripsperhour: month.map(function(d){
-              if (hoursArr[d]!== undefined){
-                return {month: d, triphour: +tripsArr[d].replace(/,/g, '') - +hoursArr[d].replace(/,/g, '')}
-              }
-            })
-           }
-         };
+                 if (t[d] !== undefined && t[d] !== '0'){
+                   var upt = trips[0][d].replace(/,/g, '');
+                   var hours = +t[d].replace(/,/g, '');
 
-         var tripspervrh = calctripspervrh(uptData, vrhData);
+                   var tripsperhour = upt/hours;
+                   return {month: d, tph: tripsperhour}
+                }
+               })
+             }
+           });
+        console.log(tripsphour);
 
-         console.log(tripspervrh)
+        deferred.resolve(tripsphour);
 
-         return uptData;
+        return deferred.promise;
        };
 
 

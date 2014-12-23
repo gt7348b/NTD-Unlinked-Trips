2d6cbf34b6e8c686c9d6a2e3e22af8380015632e
John Crocker <johnniecrocker@yahoo.com>
2014-12-23 16:24:26 -0500

    added other-files in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,11 @@ gulp.task('html', ['styles'], function () {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('other-files', function(){
+  return gulp.src('app/**/*.*')
+  .pipe(gulp.dest('dist'));
+});
+
 gulp.task('images', function () {
   return gulp.src('app/images/**/*')
     .pipe($.cache($.imagemin({

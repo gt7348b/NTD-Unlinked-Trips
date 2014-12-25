55913af1b311487ef902294ba609ef44dc3c40a8
John Crocker <johnniecrocker@yahoo.com>
2014-12-25 11:47:04 -0500

    gulp

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+console.log('Roscoes a cutie');
+
+(function(){
+
+  angular.module('UPT', ['ngResource','ngRoute'])
+    .constant({
+      'DATA_SOURCE': src='data/September 2014 Adjusted Database/UPT-Table 1.csv'
+    })
+
+
+    .config(function($routeProvider){
+
+      $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'MainController'
+      });
+
+      $routeProvider.when('/add', {
+      templateUrl: 'templates/add-template.html',
+      controller: 'AddController'
+      });
+
+      $routeProvider.when('/agency', {
+        templateUrl:'templates/agency.html',
+        controller: 'AddController'
+      });
+
+      $routeProvider.when('/vrh', {
+        templateUrl:'templates/mode.html',
+        controller:'AddController'
+      });
+
+      $routeProvider.when('/tripshour', {
+        templateUrl: 'templates/msa.html',
+        controller: 'AddController'
+      });
+
+      $routeProvider.when('/vehmiles', {
+        templateUrl: 'templates/vehmiles.html',
+        controller: 'AddController'
+      });
+
+
+
+
+    })
+
+}());

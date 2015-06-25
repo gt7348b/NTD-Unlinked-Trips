a33a0c3e93cdba681c87813818e877d8520d89ce
John Crocker <johnniecrocker@yahoo.com>
2015-06-25 14:13:45 -0400

    added to the autocorrect

--- app/scripts/controllers/add_controller.js
+++ app/scripts/controllers/add_controller.js
@@ -7,25 +7,24 @@
 
 //        console.log("hey - i'm in the add controller")
 
-
+var name_array = [];
 
     var name_search = d3.csv('data/January 2015 Adjusted Database/VRH-Table 1.csv', function(error, data){
 
-      console.log(data);
+      // console.log(data);
       var name = [];
 
       data.filter(function(entry){ //filters the data by agency name
 
-          console.log(entry.Modes);
+        //  console.log(entry.Modes);
           name.push(entry.Agency);  //adds the data to the name array
 
       });
 
-      console.log(name);
-
+      name_array = name
     });
 
-    console.log(name_search);
+    console.log(name_array);
 
         $scope.searchAgency = function(agency){
 
